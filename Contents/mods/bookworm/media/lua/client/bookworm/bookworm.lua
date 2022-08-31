b69a92bcc9503218f5b02ad65dd36cfaf557b7e6
Zefira Shannon <earthlingzephyr@gmail.com>
2022-08-31 07:35:41 -0700

    Add a basic lua version of the mod

---
+++ Contents/mods/bookworm/media/lua/client/bookworm/bookworm.lua
@@ -0,0 +1,31 @@
+local BOOKWORM = {}
+
+BOOKWORM.BuildReadMenu = function(player, context, items)
+   local isAllLiterature = true;
+   for i,v in ipairs(items) do
+      testItem = v;
+      if not instanceof(v, "InventoryItem") then
+         --print(#v.items);
+         if #v.items == 2 then
+            editItem = v.items[1];
+         end
+         testItem = v.items[1];
+      else
+         editItem = v
+      end
+      if testItem:getCategory() ~= "Literature" or testItem:canBeWrite() then
+         isAllLiterature = false;
+      end
+   end
+
+   if isAllLiterature and not getSpecificPlayer(player):getTraits():isIlliterate() then
+      ISInventoryPaneContextMenu.doLiteratureMenu(context, items, player)
+   end
+end
+
+-- Init function
+local function func_Init()
+   Events.OnFillInventoryObjectContextMenu.Add(BOOKWORM.BuildReadMenu)
+end
+
+Events.OnGameStart.Add(func_Init)
