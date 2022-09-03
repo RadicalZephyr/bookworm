a935b1d17a9822ccd949ce6f0078a1c931407cdb
Zefira Shannon <earthlingzephyr@gmail.com>
2022-09-03 03:55:05 -0700

    Don't set up on unused editItem variable

--- Contents/mods/bookworm/media/lua/client/bookworm/bookworm.lua
+++ Contents/mods/bookworm/media/lua/client/bookworm/bookworm.lua
@@ -2,16 +2,12 @@ local BOOKWORM = {}
 
 BOOKWORM.BuildReadMenu = function(player, context, items)
    local isAllLiterature = true;
+   local testItem = nil;
    local c = 0;
    for i,v in ipairs(items) do
       testItem = v;
       if not instanceof(v, "InventoryItem") then
-         if #v.items == 2 then
-            editItem = v.items[1];
-         end
          testItem = v.items[1];
-      else
-         editItem = v
       end
       if testItem:getCategory() ~= "Literature" or testItem:canBeWrite() then
          isAllLiterature = false;
