f25ecf243aa1dcea4e9647b741773f81e0fb538b
Zefira Shannon <earthlingzephyr@gmail.com>
2022-09-02 22:32:16 -0700

    Copy more code from the game with slight tweaks

--- Contents/mods/bookworm/media/lua/client/bookworm/bookworm.lua
+++ Contents/mods/bookworm/media/lua/client/bookworm/bookworm.lua
@@ -14,11 +14,29 @@ BOOKWORM.BuildReadMenu = function(player, context, items)
       end
       if testItem:getCategory() ~= "Literature" or testItem:canBeWrite() then
          isAllLiterature = false;
+         return
       end
    end
 
    if isAllLiterature and not getSpecificPlayer(player):getTraits():isIlliterate() then
-      ISInventoryPaneContextMenu.doLiteratureMenu(context, items, player)
+      BOOKWORM.doLiteratureMenu(context, items, player)
+   end
+end
+
+BOOKWORM.doLiteratureMenu = function(context, items, player)
+   local readOption = context:addOption(getText("ContextMenu_Read"), items, BOOKWORM.onLiteratureItems, player);
+   if getSpecificPlayer(player):isAsleep() then
+      readOption.notAvailable = true;
+      local tooltip = ISInventoryPaneContextMenu.addToolTip();
+      tooltip.description = getText("ContextMenu_NoOptionSleeping");
+      readOption.toolTip = tooltip;
+   end
+end
+
+BOOKWORM.onLiteratureItems = function(items, player)
+   items = ISInventoryPane.getActualItems(items)
+   for i,k in ipairs(items) do
+      ISInventoryPaneContextMenu.readItem(k, player)
    end
 end
 
