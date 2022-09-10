6ab9945fd47f725fed62ff547f1a46fd3b510108
Zefira Shannon <earthlingzephyr@gmail.com>
2022-09-09 19:29:10 -0700

    Try to rewrite the mod in typescript

--- src/client/bookworm/events.ts
+++ src/client/bookworm/events.ts
@@ -5,7 +5,7 @@
  */
 
 // PipeWrench API.
-import { getPlayer } from '@asledgehammer/pipewrench';
+import { getPlayer, KahluaTable } from '@asledgehammer/pipewrench';
 
 // PipeWrench Events API.
 import * as Events from '@asledgehammer/pipewrench-events';
@@ -15,7 +15,23 @@ import * as Events from '@asledgehammer/pipewrench-events';
 //   ISInventoryPaneContextMenu.doLiteratureMenu(context, items, player)
 // end
 
+export function buildReadMenu(player: number, context: KahluaTable, items: KahluaTable) {
+    let isAllLiterature = true;
+    let count = 0;
+    for (const [_index, v] of items) {
+        let testItem = v;
+        if !v instanceof "InventoryItem" {
+            testItem = v.items[1];
+        }
+        if testItem.getCategory() != "Literature" || testItem.canBeWrite() {
+            isAllLiterature = false;
+            return;
+        }
+        c += 1;
+    }
+
+}
 
 Events.onGameStart.addListener(() => {
-
+    Events.onFillInventoryObjectContextMenu.addListener(buildReadMenu)
 });
