b17e921317924d962ea15fccd993024063878a3f
Zefira Shannon <earthlingzephyr@gmail.com>
2022-09-14 08:01:28 -0700

    Use cat's fixed typescript syntax and mod config

--- src/client/bookworm/events.ts
+++ src/client/bookworm/events.ts
@@ -5,7 +5,7 @@
  */
 
 // PipeWrench API.
-import { getPlayer, KahluaTable } from '@asledgehammer/pipewrench';
+import { getPlayer, KahluaTable, InventoryItem } from '@asledgehammer/pipewrench';
 
 // PipeWrench Events API.
 import * as Events from '@asledgehammer/pipewrench-events';
@@ -20,14 +20,14 @@ export function buildReadMenu(player: number, context: KahluaTable, items: Kahlu
     let count = 0;
     for (const [_index, v] of items) {
         let testItem = v;
-        if !v instanceof "InventoryItem" {
+        if (!(v instanceof InventoryItem)) {
             testItem = v.items[1];
         }
-        if testItem.getCategory() != "Literature" || testItem.canBeWrite() {
+        if (testItem.getCategory() != "Literature" || testItem.canBeWrite()) {
             isAllLiterature = false;
             return;
         }
-        c += 1;
+        count += 1;
     }
 
 }
